892345b6e3b406b27dca5ce6baa61dbaff3ea374
Katherine Cox <katherinecox@jhu.edu>
2021-03-02 17:26:51 -0500

    Adding some skeleton documentation

--- R/marianes_htseq.R
+++ R/marianes_htseq.R
@@ -1,1 +1,96 @@
+#' HTSeq readcounts from regions of the Drosophila midgut
+#'
+#' Fill in more documentation
+#'
+"marianes_htseq_metadata"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  a1-3 region
 "marianes_htseq_am1"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  a1-3 region
+"marianes_htseq_am2"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  a1-3 region
+"marianes_htseq_am3"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  CuLFCFe region
+"marianes_htseq_am4"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  CuLFCFe region
+"marianes_htseq_am5"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  CuLFCFe region
+"marianes_htseq_am6"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  p1-4 region
+"marianes_htseq_am7"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  p1-4 region
+"marianes_htseq_am8"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  p1-4 region
+"marianes_htseq_am9"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  a1 region
+"marianes_htseq_am10"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  a1 region
+"marianes_htseq_am11"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  a1 region
+"marianes_htseq_am12"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  a2-3 region
+"marianes_htseq_am13"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  a2-3 region
+"marianes_htseq_am14"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  a2-3 region
+"marianes_htseq_am15"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  Cu region
+"marianes_htseq_am16"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  Cu region
+"marianes_htseq_am17"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  Cu region
+"marianes_htseq_am18"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  LFCFe region
+"marianes_htseq_am19"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  LFCFe region
+"marianes_htseq_am20"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  LFCFe region
+"marianes_htseq_am21"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  Fe region
+"marianes_htseq_am22"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  Fe region
+"marianes_htseq_am23"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  Fe region
+"marianes_htseq_am24"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  p1 region
+"marianes_htseq_am25"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  p1 region
+"marianes_htseq_am26"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  p1 region
+"marianes_htseq_am27"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  p2-4 region
+"marianes_htseq_am28"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  p2-4 region
+"marianes_htseq_am29"
+
+#' @describeIn marianes_htseq_metadata Individual replicate from the  p2-4 region
+"marianes_htseq_am30"
+
--- data-raw/marianes_htseq/process_marianes_htseq.R
+++ data-raw/marianes_htseq/process_marianes_htseq.R
@@ -1,15 +1,25 @@
 ## code to prepare `marianes_htseq` dataset goes here
 
-meta <- read.table("data-raw/marianes_htseq/midgut.tsv", header=TRUE, sep="\t")
-
+marianes_htseq_metadata <- read.table("data-raw/marianes_htseq/midgut.tsv", header=TRUE, sep="\t")
+usethis::use_data(marianes_htseq_metadata, overwrite=TRUE)
 # see https://stackoverflow.com/questions/59977966/how-to-dynamically-change-names-inside-a-for-loop-in-usethisuse-data-r
 
-for (row in 1:nrow(meta)) {
-  sample_name <- paste0("marianes_htseq_", meta[row, "sample"])
-  data  <- read.table(paste0("data-raw/marianes_htseq/", meta[row, "filename"]), sep="\t")
+cat("#' HTSeq readcounts from regions of the Drosophila midgut
+#'
+#' Fill in more documentation
+#'
+\"marianes_htseq_metadata\"\n\n", file="R/marianes_htseq.R")
+
+for (row in 1:nrow(marianes_htseq_metadata)) {
+  sample_name <- paste0("marianes_htseq_", marianes_htseq_metadata[row, "sample"])
+  cat("#' @describeIn marianes_htseq_metadata Individual replicate from the ", marianes_htseq_metadata[row, "condition"], "region\n", file="R/marianes_htseq.R", append=TRUE)
+  cat("\"", sample_name, "\"\n\n", file="R/marianes_htseq.R", sep="", append=TRUE)
+  data  <- read.table(paste0("data-raw/marianes_htseq/", marianes_htseq_metadata[row, "filename"]), sep="\t")
   assign(sample_name, data)
   do.call(usethis::use_data, list(as.name(sample_name), overwrite = TRUE))
 }
+
+
 
 
 
